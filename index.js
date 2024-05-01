b7e089b2847cc0cbba6a5b28162623f00387dfa2
Devendra Royal <94845156+0xdev02@users.noreply.github.com>
2024-05-01 09:17:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ function updateData() {
     fetch('https://api.dexscreener.com/latest/dex/pairs/ethereum/0x76366D95c2016446247296Ea50c8D06D0585ae00')
         .then(response => response.json())
         .then(data => {
-            const priceNative = data.pairs.priceUsd;
-            const priceChange = data.pairs.priceChange.h24;
+            const priceUsd = data.pairs[0].priceUsd;
+            const priceChange = data.pairs[0].priceChange.h24;
 
 
             priceElement.textContent = priceUsd;  // Update the HTML element
